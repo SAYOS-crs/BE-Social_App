27194b5a696894c388caf9854e8adade7691f0ed
SAYOS-crs <eslam.mohamed.ki123@gmail.com>
2026-08-28 18:41:31 +0300

    feat: AWS enum added

---
+++ src/Utils/Enums/AWS.enum.ts
@@ -0,0 +1,13 @@
+export enum AssetType {
+  Profile = "Profile",
+  Cover = "Cover",
+  Images = "Images",
+  Docs = "Docs",
+  attachments = "attachments",
+}
+
+export enum FolderType {
+  User = "User",
+  Post = "Post",
+  Comments = "Comments",
+}
--- src/Utils/Enums/index.ts
+++ src/Utils/Enums/index.ts
@@ -1,3 +1,4 @@
 export * as Enums from "./enum";
 export * from "./enum"; // named exports — allows: import { TokenType, Rolle, ... }
 export * as PostEnum from "./PostEnums";
+export * as AwsEnum from "./AWS.enum";
